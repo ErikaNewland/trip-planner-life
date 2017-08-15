4dc462048bc47773a42970d393b9b08cae1d45c8
Erika Newland <erika.s.newland@gmail.com>
2017-08-15 16:46:28 -0400

    working on the map

--- public/js/main.js
+++ public/js/main.js
@@ -12,14 +12,16 @@ function initialize_gmaps() {
     // initialize a new Google Map with the options
     var map = new google.maps.Map(map_canvas_obj, mapOptions);
     // Add the marker to the map
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title:"Hello World!"
-    });
+    // var marker = new google.maps.Marker({
+    //     position: myLatlng,
+    //     title:"Hello World!"
+    // });
     // Add the marker to the map by calling setMap()
-    marker.setMap(map);
+//     marker.setMap(map);
 }
 
 $(document).ready(function() {
     initialize_gmaps();
 });
+
+///////////////////////
